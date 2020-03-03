ce4cf6fbae335efc406e02c76cd4674b2fedb8d6
FLORES SANCHEZ <salvador.flores-sanchez@capgemini.com>
2020-03-02 18:27:53 -0600

    Remove ribbon, implementing eureka-client

--- src/main/java/mx/com/springboot/app/items/SpringbootServicioItemApplication.java
+++ src/main/java/mx/com/springboot/app/items/SpringbootServicioItemApplication.java
@@ -3,11 +3,10 @@ package mx.com.springboot.app.items;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.cloud.netflix.ribbon.RibbonClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @EnableEurekaClient
-@RibbonClient(name = "servicio-productos")
+//@RibbonClient(name = "servicio-productos") //Esta se comenta porque se metio el eureka-client
 @EnableFeignClients
 @SpringBootApplication
 public class SpringbootServicioItemApplication {
