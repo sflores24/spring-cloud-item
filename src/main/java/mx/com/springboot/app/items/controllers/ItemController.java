c8d216181b959652f4767bbbe77aada5e2e91d67
sflores24 <flores.salvador@gmail.com>
2020-03-03 15:12:58 -0600

    add profiles

--- src/main/java/mx/com/springboot/app/items/controllers/ItemController.java
+++ src/main/java/mx/com/springboot/app/items/controllers/ItemController.java
@@ -1,9 +1,17 @@
 package mx.com.springboot.app.items.controllers;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.env.Environment;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,33 +24,56 @@ import mx.com.springboot.app.items.models.service.IItemService;
 
 @RestController
 public class ItemController {
+	Logger log = LoggerFactory.getLogger(ItemController.class);
+
+	@Autowired
+	private Environment env;
+
+	@Value("${configuracion.texto}")
+	private String texto;
 
 	@Autowired
 	@Qualifier("serviceFeign")
-	//@Qualifier("serviceRestTemp")
+	// @Qualifier("serviceRestTemp")
 	private IItemService itemService;
-	
+
+	@GetMapping("/obtener-config")
+	public ResponseEntity<?> obtenerConfiguracion(@Value("${server.port}") String puerto) {
+		Map<String, String> json = new HashMap<String, String>();
+		json.put("texto", texto);
+		json.put("puerto", puerto);
+
+		if (env.getActiveProfiles() != null && env.getActiveProfiles().length > 0
+				&& env.getActiveProfiles()[0].equals("dev")) {
+			json.put("autor.nombre", env.getProperty("configuracion.autor.nombre"));
+			json.put("autor.email", env.getProperty("configuracion.autor.email"));
+		}
+
+		// json va en el body
+		return new ResponseEntity<Map<String, String>>(json, HttpStatus.OK);
+	}
+
 	@GetMapping("/listar")
 	public List<Item> listar() {
 		return itemService.findAll();
 	}
-	
+
 	@HystrixCommand(fallbackMethod = "metodoAlternativo")
 	@GetMapping("/ver/{id}/cantidad/{cantidad}")
 	public Item detalle(@PathVariable Long id, @PathVariable Integer cantidad) {
 		return itemService.findById(id, cantidad);
 	}
-	
+
 	public Item metodoAlternativo(Long id, Integer cantidad) {
 		Item item = new Item();
 		Producto producto = new Producto();
-		
+
 		item.setCantidad(1);
 		producto.setId(id);
 		producto.setNombre("alternativo");
 		producto.setPrecio(200.00);
 		item.setProducto(producto);
-		
+
 		return item;
 	}
 }
